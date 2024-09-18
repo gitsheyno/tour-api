ff6e57ce3693486c7c8576677f52e82aa8964956
shayan.heidary <shayan.heidary@knime.com>
2024-09-18 10:35:31 +0200

    connect to mango

---
+++ index.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+import express from 'express';
+import { model, Schema, Model, Document,connect, connection } from 'mongoose';
+
+
+dotenv.config();
+
+console.log(process.env.DATABASE_PASSWORD)
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    res.send("hello there ...")
+})
+
+const DB = process.env.DATABASE_URL?.replace('<db_password>',process.env.DATABASE_PASSWORD as string);
+
+connect(DB as string).then(()=>{
+    console.log('Connection erfolg')
+})
+
+app.listen(port,()=>{
+console.log(`form ${port}`)
+})
