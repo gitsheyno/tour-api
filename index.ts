46faf6a99396affc0bb3cf0cf43ebdd73ffe9a79
shayan.heidary <shayan.heidary@knime.com>
2024-09-18 10:53:10 +0200

    first schmea

--- index.ts
+++ index.ts
@@ -1,24 +1,44 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import express from 'express';
-import { model, Schema, Model, Document,connect, connection } from 'mongoose';
-
+import { model, Schema, Model, Document, connect } from 'mongoose';
 
 dotenv.config();
 
-console.log(process.env.DATABASE_PASSWORD)
+console.log(process.env.DATABASE_PASSWORD);
 const app = express();
 const port = 3000;
 
-app.get("/",(req,res)=>{
-    res.send("hello there ...")
-})
+app.get('/', (req, res) => {
+  res.send('hello there ...');
+});
 
-const DB = process.env.DATABASE_URL?.replace('<db_password>',process.env.DATABASE_PASSWORD as string);
+const DB = process.env.DATABASE_URL?.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD as string
+);
 
-connect(DB as string).then(()=>{
-    console.log('Connection erfolg')
-})
+connect(DB as string).then(() => {
+  console.log('Connection erfolg');
+});
 
-app.listen(port,()=>{
-console.log(`form ${port}`)
-})
+const toursSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'a tour must have a price'],
+  },
+});
+
+const Tour = model('Tour', toursSchema);
+
+app.listen(port, () => {
+  console.log(`form ${port}`);
+});
