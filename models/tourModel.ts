f0f11f56d3c6588232a8811df12442610ae95550
shayan.heidary <shayan.heidary@knime.com>
2024-09-25 01:41:56 +0200

    modify tour model - imported data

--- models/tourModel.ts
+++ models/tourModel.ts
@@ -4,22 +4,72 @@ export interface ITour {
   name?: string;
   rating: number;
   price: number;
+  duration: number;
+  maxGroupSize: number;
+  difficulty: string;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  priceDiscount: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  createdAt: Date;
+  startDates: Date[];
 }
 
 const toursSchema = new Schema<ITour>({
   name: {
     type: String,
-    required: true,
+    required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: [true, 'a tour must have a price'],
+    required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  startDates: [Date],
 });
 
 const Tour = model<ITour>('Tour', toursSchema);
